5ca42d64f601b4353f933d1c05707ccae594c04c
Ausanc <henrygilbert15@live.co.uk>
2017-12-15 22:33:16 +0000

    Add converting a test image to greyscale and 48x48

---
+++ main.py
@@ -0,0 +1,28 @@
+from PIL import Image
+
+
+# convert an input image into a minimised greyscale image
+def create_minimised_image(input_image):
+    image = input_image.convert("L")
+    return image.resize((48, 48))
+
+
+# TODO: create a template for building from a minimised image
+def min_to_template(min_image):
+    return min_image.resize((480, 480), Image.NEAREST)  # placeholder
+
+
+# TODO: convert a greyscale minimised image to the lego colour palette
+def convert_to_lego_colors(min_image):
+    pass
+
+
+def main():
+    image = Image.open("testimg.jpg")
+    image = create_minimised_image(image)
+    image = min_to_template(image)
+    image.save("output.jpg")
+
+
+if __name__ == '__main__':
+    main()
