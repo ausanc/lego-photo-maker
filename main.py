30f6ab767eec1ede37880d1bee62ba17dffe00be
Ausanc <henrygilbert15@live.co.uk>
2017-12-20 09:53:04 +0000

    Refactor greyscale functions to use an arbitrary palette

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import argparse
-import sys
+from colormath.color_objects import sRGBColor, LabColor
+from colormath.color_conversions import convert_color
+from colormath.color_diff import delta_e_cie2000
 from PIL import Image
 
-# TODO: fix output image colors to match proper lego colors
-# TODO: crop input images to square
 # TODO: export options: baseplate, tiles instead of plates
 # TODO: saturation option
 # TODO: simple background removal somehow and replace with yellow studs
 
 
-# convert an input image into a minimised greyscale image
-def create_minimised_image(input_image):
-    image = input_image.convert("L")
+# convert an input image into a minimised image
+def create_minimised_image(image):
     return image.resize((48, 48))
 
 
@@ -20,31 +19,35 @@ def min_to_template(min_image):
     return min_image.resize((480, 480), Image.NEAREST)  # placeholder
 
 
-# TODO: replace this with something which doesn't suck
-def get_lego_color(color):
-    # F2F3F2, A3A2A4, 635F61, 1B2A34
-    # greyscale: 242, 162, 96, 38
-    # mid-points: 202, 129, 67
-    if color > 202:
-        return 242
-    elif color > 129:
-        return 162
-    elif color > 67:
-        return 96
-    else:
-        return 38
-
-
-# convert a greyscale minimised image to the lego colour palette
-def convert_to_lego_colors(min_image):
-    counters = {"242": 0, "162": 0, "96": 0, "38": 0}
-    width, height = min_image.size
+# takes an image and converts it to be in the provided palette
+def convert_image_to_palette(image, palette):
+    width, height = image.size
+    rgb_colors = {}
+    for key in palette.keys():
+        c = palette[key]
+        rgb_colors[key] = sRGBColor(c[0], c[1], c[2], is_upscaled=True)
+    # rgb_colors = {key: sRGBColor(c[0], c[1], c[2], is_upscaled=True) for key, c in palette}
+    lab_colors = {}
+    for key in rgb_colors.keys():
+        c = rgb_colors[key]
+        lab_colors[key] = convert_color(c, LabColor)
+    # lab_colors = {key: convert_color(c, LabColor) for key, c in rgb_colors}
     converted = Image.new('RGB', (width, height))
-    for h in range(height):
-        for w in range(width):
-            c = get_lego_color(min_image.getpixel((w, h)))
-            counters[str(c)] = counters[str(c)] + 1
-            converted.putpixel((w, h), (c, c, c))
+
+    counters = {}
+    for w in range(width):
+        for h in range(height):
+            r, g, b = image.getpixel((w, h))
+            pixel_color = convert_color(sRGBColor(r, g, b, is_upscaled=True), LabColor)
+            differences = {}
+            for key in lab_colors.keys():
+                c = lab_colors[key]
+                differences[key] = delta_e_cie2000(pixel_color, c)
+            # differences = {key: delta_e_cie2000(pixel_color, c) for key, c in lab_colors}
+            min_key = min(differences, key=differences.get)
+            converted.putpixel((w, h), palette[min_key])
+            counters[min_key] = counters.get(min_key, 0) + 1
+    print(counters)
     return converted, counters
 
 
@@ -62,22 +65,28 @@ def main():
     parser.add_argument('image', help='the image to be converted')
     args = parser.parse_args()
 
+    greyscale_palette = {"white": (242, 243, 242),
+                         "lbley": (163, 162, 164),
+                         "dbley": (99, 95, 97),
+                         "black": (27, 42, 52)}
+
     image = Image.open(args.image)
     image = crop_to_square(image)
     image = create_minimised_image(image)
-    image, counters = convert_to_lego_colors(image)
+    image, counters = convert_image_to_palette(image, greyscale_palette)
     image = min_to_template(image)
     image.save("output.jpg")
 
-    template_counters = {"white": str(counters["242"]),
-    "lbley": str(counters["162"]),
-    "dbley": str(counters["96"]),
-    "black": str(counters["38"])}
-
-    with open("template.xml", "r") as f:
-        template = f.read().format(**template_counters)
-        with open("output.xml", "w") as o:
-            o.write(template)
+    # rewrite this section to work with new, generic palette system
+    # template_counters = {"white": str(counters["white"]),
+    #                      "lbley": str(counters["lbley"]),
+    #                      "dbley": str(counters["dbley"]),
+    #                      "black": str(counters["black"])}
+    #
+    # with open("template.xml", "r") as f:
+    #     template = f.read().format(**template_counters)
+    #     with open("output.xml", "w") as o:
+    #         o.write(template)
 
 
 if __name__ == '__main__':
